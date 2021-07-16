01356bfc9e3e01bbfddbb9eb799b753c7a0d5c07
Maksym Doshchenko <doschenko54@gmail.com>
2021-07-16 20:32:29 +0300

    fixed

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
 let formInput = document.querySelector('.form__input');
 let formBtn = document.querySelector('.form__btn');
 let list = document.querySelector('.todo__list');
-let ourLi = [];
-/* formBtn.addEventListener('click', () => {
-    let li = document.createElement('li')
-    li.classList = 'list__item';
-    li.innerText = `${formInput.value}`;
-    list.append(li);
-}); */
+
+
 formBtn.addEventListener('click', () => {
-    let newElem = document.createElement('li');
-    newElem.classList.add('todo__list-item');
-    newElem.innerText = `${formInput.value}`;
-    list.append(newElem);
-    newElem.addEventListener('click', () => {
-        newElem.classList.toggle('active');
-    });
-    let newElem2 = document.createElement('div');
-    newElem2.classList.add('cancel');
-    newElem.append(newElem2);
-    newElem2.addEventListener('click', (event) => {
-        if (event.target.tagName === 'DIV'){
-            newElem.remove();
-        }
-    });
+    let li = list.innerHTML;
+    li += `<li class="todo__list-item">${formInput.value}<div class="cancel"></div></li>`;
+    list.innerHTML = li;
 });
 
+
+
+list.addEventListener('click', (event) => {
+    if (event.target.tagName === 'DIV'){
+        event.target.closest('.todo__list-item').remove();
+    } else if (event.target.tagName === "LI") {
+        event.target.classList.toggle('active');
+    }
+});
